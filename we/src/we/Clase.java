0bfc76befe730b4c382f9787e6f2dacb7dc55e2c
diego <diego@dpc>
2013-09-26 21:21:39 -0600

    uno

---
+++ we/src/we/Clase.java
@@ -0,0 +1,17 @@
+package we;
+
+public class Clase {
+
+	public Clase() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
